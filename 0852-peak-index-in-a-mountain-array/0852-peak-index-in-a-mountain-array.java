d24e303f11682018c673c5767e414b084dc8bf6d
Birajadar Annasaheb Ramagonda <157781556+ezi0op@users.noreply.github.com>
2026-05-27 10:24:52 +0530

    Time: 0 ms (100%), Space: 80.5 MB (20.11%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] a) {
+        int s=0;
+        int e=a.length-1;
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(a[m]>a[m+1]){
+                e=m;
+            }else{
+                s=m+1;
+            }
+        }
+            return s;
+    }
+
+}
