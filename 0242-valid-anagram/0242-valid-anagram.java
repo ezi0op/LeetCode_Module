f69990c042369ec5a83ed60213fdbd33eba5c2a8
Birajadar Annasaheb Ramagonda <157781556+ezi0op@users.noreply.github.com>
2026-06-11 15:24:43 +0530

    Time: 2218 ms (5.65%), Space: 44.9 MB (50.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+
+       
+        boolean[] flag = new boolean[b.length];
+
+        for (int i = 0; i < a.length; i++) {
+            boolean f=false;
+
+            for (int j = 0; j < b.length; j++) {
+                if (!flag[j]&&a[i] == b[j]) {
+                    f=true;
+                    flag[j] = true;
+                    break;
+                }
+            }
+            if(!f){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
