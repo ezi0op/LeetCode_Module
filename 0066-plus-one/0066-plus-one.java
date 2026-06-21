c2254da254cef9cd64cccff807704d26658f7c05
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2026-06-21 09:33:06 +0530

    Time: 0 ms (100%), Space: 43.4 MB (60.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,22 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int[] a;
-        for (int i = digits.length-1; i >=0 ; i--) {
-            
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i]=0;
+     for (int i = digits.length-1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
 
-           
-        }
-        int[] res=new int[digits.length+1];
-        res[0]=1;
-
-         return res;
+			}
+			digits[i] = 0;
+		}
+		int ans[] = new int[digits.length + 1];
+		ans[0] = 1;
+		return ans;   
     }
-
-       
-    
 }
