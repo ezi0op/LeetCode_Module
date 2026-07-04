180c0150c96c1b44f0c86d74e32d9afe599f22c8
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2026-07-04 09:01:46 +0530

    Time: 3 ms (7.59%), Space: 46.4 MB (5.88%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+
+		return permute(new ArrayList<>(), nums);
+	}
+
+	static List<List<Integer>> permute(List<Integer> p, int[] up) {
+		if (up.length == 0) {
+			List<List<Integer>> ans = new ArrayList<>();
+			ans.add(new ArrayList<>(p));
+			return ans;
+		}
+		int current = up[0];
+		List<List<Integer>> ans = new ArrayList<>();
+		for (int i = 0; i <= p.size(); i++) {
+			List<Integer> first = new ArrayList<>(p.subList(0, i));
+			List<Integer> second = new ArrayList<>(p.subList(i, p.size()));
+			List<Integer> temp = new ArrayList<>();
+			temp.addAll(first);
+			temp.add(current);
+			temp.addAll(second);
+			ans.addAll(permute(temp, Arrays.copyOfRange(up, 1, up.length)));
+		}
+		return ans;
+	}
+}
