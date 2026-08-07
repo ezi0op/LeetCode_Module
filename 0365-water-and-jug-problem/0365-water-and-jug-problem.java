53ca3aa01afb2468a5f22cc51f86df64b5eb6de4
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2026-08-07 08:01:15 +0530

    Time: 0 ms (100%), Space: 41.9 MB (89.27%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+	if (target > x + y) {
+			return false;
+		}
+		if (target == 0) {
+			return true;
+		}
+		return target % gcd(x, y) == 0;
+
+	}
+
+	private static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+}
