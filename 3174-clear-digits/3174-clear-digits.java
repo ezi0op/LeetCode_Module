f8156e9fbe9a37c68cf3080b3e7dc623946727f7
Annasaheb Ramagonda Birajadar <157781556+ezi0op@users.noreply.github.com>
2026-08-08 08:29:08 +0530

    Time: 3 ms (40.71%), Space: 44.2 MB (22.68%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String clearDigits(String s) {
+     	Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (Character.isDigit(c)) {
+				if (!stack.isEmpty()) {
+					stack.pop();
+				}
+			} else {
+				stack.push(c);
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (Character c : stack) {
+			sb.append(c);
+		}
+
+		return sb.toString();
+
+	}
+    
+}
